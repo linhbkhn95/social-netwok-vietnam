78c07c661fdf9e7b4ff6d267b3f3cff6a2d04218
linhtd <trinhducbaolinh@gmail.com>
2018-11-03 13:33:16 +0700

    update modal status

--- app/components/pages/group/components/home/LayoutHome.js
+++ app/components/pages/group/components/home/LayoutHome.js
@@ -1,32 +1,24 @@
-import React from 'react';
-import HeaderListPost from './components/HeaderListPost'
-import HeaderPost from './HeaderPost'
-import ListPost from './ListPost'
-import Relative from '../Relative'
+import React from "react";
+import HeaderListPost from "./components/HeaderListPost";
+import ModalPostStatus from "app/utils/modal/ModalPostStatus";
+import ListPost from "./ListPost";
+import Relative from "../Relative";
 
-import {NavLink,Route} from 'react-router-dom';
+import { NavLink, Route } from "react-router-dom";
 
-class Wall extends React.Component{
-
-  render(){
-     return(
-
-        <div style={{paddingLeft:"0px"}} className="col-md-12">
-
-                    <HeaderPost username={this.props.username} />
-                    <div className="col-md-12 list-post " >
-                    <div className="">
-                        {/* <HeaderListPost /> */}
-                        <div>
-                            {this.props.children}
-                        </div>
-
-                    </div>
-                    </div>
-
+class Wall extends React.Component {
+  render() {
+    return (
+      <div style={{ paddingLeft: "0px" }} className="col-md-12">
+        <ModalPostStatus groupname={this.props.groupname} />
+        <div className="col-md-12 list-post ">
+          <div className="">
+            {/* <HeaderListPost /> */}
+            <div>{this.props.children}</div>
+          </div>
         </div>
-
-     )
+      </div>
+    );
   }
 }
-module.exports =  Wall;
+module.exports = Wall;
